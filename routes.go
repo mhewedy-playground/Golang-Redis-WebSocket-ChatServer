87b00cd5908d0c46882dd3bb78b723e8e4b681dc
Muhammad Hewedy <mhewedy@gmail.com>
2020-05-23 18:49:34 +0300

    implement connect subscribe and unsubscribe rest apis

--- routes.go
+++ routes.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"fmt"
 	"github.com/go-redis/redis/v7"
+	"github.com/gorilla/mux"
 	"net/http"
 )
 
@@ -12,13 +14,35 @@ func h(rdb *redis.Client, fn func(http.ResponseWriter, *http.Request, *redis.Cli
 }
 
 func connectHandler(w http.ResponseWriter, r *http.Request, rdb *redis.Client) {
+	username := mux.Vars(r)["user"]
 
+	if err := newUser(username).connect(rdb); err != nil {
+		handelError(err, w)
+		return
+	}
 }
 
 func subscribeHandler(w http.ResponseWriter, r *http.Request, rdb *redis.Client) {
+	username := mux.Vars(r)["user"]
+	channel := mux.Vars(r)["channel"]
 
+	if err := newUser(username).subscribe(rdb, channel); err != nil {
+		handelError(err, w)
+		return
+	}
 }
 
 func unsubscribeHandler(w http.ResponseWriter, r *http.Request, rdb *redis.Client) {
+	username := mux.Vars(r)["user"]
+	channel := mux.Vars(r)["channel"]
 
+	if err := newUser(username).unsubscribe(rdb, channel); err != nil {
+		handelError(err, w)
+		return
+	}
+}
+
+func handelError(err error, w http.ResponseWriter) {
+	w.WriteHeader(http.StatusBadRequest)
+	w.Write([]byte(fmt.Sprintf(`{"error": "%s"}`, err.Error())))
 }
